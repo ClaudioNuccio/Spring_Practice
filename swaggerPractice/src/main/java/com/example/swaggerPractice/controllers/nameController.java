84af3dc6dd82fca9915d0f54cf80a02b0b0a5bf4
Claudio Nuccio <cnuccio00@gmail.com>
2024-01-20 17:43:08 +0100

    restored

--- swaggerPractice/src/main/java/com/example/swaggerPractice/controllers/nameController.java
+++ swaggerPractice/src/main/java/com/example/swaggerPractice/controllers/nameController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("v1/")
 public class nameController {
-    @GetMapping(path ="Name")
-    public String getName (@RequestParam String name){
+    @GetMapping(path = "Name")
+    public String getName(@RequestParam String name) {
         return name;
     }
-    @PostMapping(path ="Name")
-    public StringBuilder postName (@RequestParam String name){
-        StringBuilder invertedName = new StringBuilder(name);
-        return  invertedName.reverse();
+
+    @PostMapping(path = "Name")
+    public String postName(@RequestParam String name) {
+        return name;
     }
 }
