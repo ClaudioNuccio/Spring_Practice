1007492bf1718852b57800e1d248274a4dab4d63
Claudio Nuccio <cnuccio00@gmail.com>
2024-01-20 18:40:16 +0100

    created

---
+++ corsPractice/src/test/java/com/example/corsPractice/CorsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corsPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
