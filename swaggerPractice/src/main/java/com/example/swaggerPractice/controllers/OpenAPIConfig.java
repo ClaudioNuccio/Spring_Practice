21bc455150c474024d7f43a4b59fef04f29f1c90
Claudio Nuccio <cnuccio00@gmail.com>
2024-01-21 12:40:15 +0100

    added first documentations

---
+++ swaggerPractice/src/main/java/com/example/swaggerPractice/controllers/OpenAPIConfig.java
@@ -0,0 +1,51 @@
+package com.example.swaggerPractice.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+//Add urls in application.properties
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("${swaggerPractice.openapi.dev-url}")
+    private String devUrl;
+
+    @Value("${swaggerPractice.openapi.prod-url}")
+    private String prodUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Server prodServer = new Server();
+        prodServer.setUrl(prodUrl);
+        prodServer.setDescription("Server URL in Production environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("nuccio.claudio98@gmail.com");
+        contact.setName("Nuccio Claudio");
+        contact.setUrl("https://github.com/ClaudioNuccio/ClaudioNuccio");
+
+        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("Tutorial Management API")
+                .version("1.0")
+                .contact(contact)
+                .description("This API return username with get API and reverse it with a  POST")
+                .termsOfService("https://it.wikipedia.org/wiki/Licenza_MIT")
+                .license(mitLicense);
+
+        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+    }
+}
--- swaggerPractice/src/main/java/com/example/swaggerPractice/controllers/nameController.java
+++ swaggerPractice/src/main/java/com/example/swaggerPractice/controllers/nameController.java
@@ -1,7 +1,9 @@
 package com.example.swaggerPractice.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Practice", description = "A practice controller to write with Swagger following OpenAPI standards")
 @RestController
 @RequestMapping("v1/")
 public class nameController {
@@ -11,7 +13,8 @@ public class nameController {
     }
 
     @PostMapping(path = "Name")
-    public String postName(@RequestParam String name) {
-        return name;
+    public StringBuilder postName(@RequestParam String name) {
+        StringBuilder invertedName = new StringBuilder(name);
+        return  invertedName.reverse();
     }
 }
