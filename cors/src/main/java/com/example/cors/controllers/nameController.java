1a2a4754905e3d9c6d67a74210788d0f5e7d9f64
Claudio Nuccio <cnuccio00@gmail.com>
2024-01-21 18:19:13 +0100

    added cors

---
+++ cors/src/main/java/com/example/cors/controllers/nameController.java
@@ -0,0 +1,18 @@
+package com.example.cors.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("v1/")
+public class nameController {
+    @GetMapping(path = "Name")
+    public String getName(@RequestParam String name) {
+        return name;
+    }
+
+    @PostMapping(path = "Name")
+    public StringBuilder postName(@RequestParam String name) {
+        StringBuilder invertedName = new StringBuilder(name);
+        return  invertedName.reverse();
+    }
+}
