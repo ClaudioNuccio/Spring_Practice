65a74f786080dabfecff6469162e693227331b74
Claudio Nuccio <cnuccio00@gmail.com>
2024-01-23 09:57:57 +0100

    fixed names

--- controllerGetPost/src/main/java/controllerGetPost/controllerGetPost/nameController.java
+++ controllerGetPost/src/main/java/controllerGetPost/controllerGetPost/nameController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("v1/")
     public class nameController {
-    @GetMapping(path ="Name")
+    @GetMapping(path ="getName")
     public String getName (@RequestParam String name){
         return name;
     }
-    @PostMapping(path ="Name")
+    @PostMapping(path ="createReversedName")
     public StringBuilder postName (@RequestParam String name){
         StringBuilder invertedName = new StringBuilder(name);
         return  invertedName.reverse();
