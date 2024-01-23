1b93b954c5eb31b5941075b46472b192a6027ae0
Claudio Nuccio <cnuccio00@gmail.com>
2024-01-23 11:38:36 +0100

    Fixed naming

--- cors/src/main/java/com/example/cors/controllers/nameController.java
+++ cors/src/main/java/com/example/cors/controllers/nameController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("v1/")
 public class nameController {
-    @GetMapping(path = "getName")
+    @GetMapping(path = "getname")
     public String getName(@RequestParam String name) {
         return name;
     }
 
-    @PostMapping(path = "createReversedName")
+    @PostMapping(path = "createreversedname")
     public StringBuilder postName(@RequestParam String name) {
         StringBuilder invertedName = new StringBuilder(name);
         return  invertedName.reverse();
