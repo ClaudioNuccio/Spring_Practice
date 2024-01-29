0fd3b51fafede1f3db2e6d6e7d9a5635a585c924
Claudio Nuccio <cnuccio00@gmail.com>
2024-01-29 20:39:37 +0100

    CRUD

--- crud-exercise/src/main/java/com/example/crudexercise/controllers/CarController.java
+++ crud-exercise/src/main/java/com/example/crudexercise/controllers/CarController.java
@@ -1,15 +1,50 @@
 package com.example.crudexercise.controllers;
 
+import com.example.crudexercise.entities.Car;
+import com.example.crudexercise.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/car")
 public class CarController {
-    @PostMapping
+    @Autowired
+    CarRepository carRepository;
+    @PostMapping(path = "create")
+    public Car createCar (@RequestBody Car car){
+        return carRepository.saveAndFlush(car);
+    }
+    @GetMapping(path = "/read/{id}")
+    public Car readCar (@PathVariable long id){
+        Car car = null;
+        if(!carRepository.existsById(id)){
+            return car;
+        }
+        else {
+            return carRepository.getById(id);
+        }
+    }
 
-    @GetMapping
+    @GetMapping(path = "/readall")
+    public List<Car> readCars (){
+        return carRepository.findAll();
+    }
 
-    @PutMapping
 
-    @DeleteMapping
+    @PutMapping(path = "/update/{id}")
+    public Car updateCar(@PathVariable long id, @RequestBody Car car){
+        car.setId(id);
+        return carRepository.saveAndFlush(car);
+    }
+    @DeleteMapping(path = "deleteone/{id}")
+    public void deleteOne (@PathVariable long id){
+        carRepository.deleteById(id);
+    }
+    @DeleteMapping(path = "deleteall/{id}")
+    public void deleteAll (@PathVariable long id){
+        carRepository.deleteAllById(Collections.singleton(id));
+    }
 }
--- crud-exercise/src/main/java/com/example/crudexercise/entities/Car.java
+++ crud-exercise/src/main/java/com/example/crudexercise/entities/Car.java
@@ -1,14 +1,14 @@
 package com.example.crudexercise.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
 @Entity
 @Table
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +16,28 @@ public class Car {
 
     private String modelName;
     private String type;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
