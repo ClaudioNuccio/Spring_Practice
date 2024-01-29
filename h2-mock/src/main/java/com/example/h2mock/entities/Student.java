f6c60a8b525a84caa9bddd5a8ed8d958ee533c46
Claudio Nuccio <cnuccio00@gmail.com>
2024-01-29 19:15:15 +0100

    Added H2Mock

---
+++ h2-mock/src/main/java/com/example/h2mock/entities/Student.java
@@ -0,0 +1,27 @@
+package com.example.h2mock.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long student_id;
+    @Column(nullable = false)
+    private String first_Name;
+    @Column(nullable = false)
+    private String last_name;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
