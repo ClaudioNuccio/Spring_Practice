53a9b4227f7c3c5706ad8ae94bc4b19a6f02bb78
Claudio Nuccio <cnuccio00@gmail.com>
2024-02-01 21:06:32 +0100

    fixed

--- repository1/src/main/java/com/example/repository1/repositories/CarRepository.java
+++ repository1/src/main/java/com/example/repository1/repositories/CarRepository.java
@@ -4,6 +4,8 @@ import com.example.repository1.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CarRepository extends JpaRepository<Car,Long> {
 }
