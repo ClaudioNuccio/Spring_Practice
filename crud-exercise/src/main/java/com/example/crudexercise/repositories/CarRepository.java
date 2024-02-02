ce01c1aec4822eaff1843c765e2a60bc9f2150e7
Claudio Nuccio <cnuccio00@gmail.com>
2024-02-02 14:53:50 +0100

    fixed

--- crud-exercise/src/main/java/com/example/crudexercise/repositories/CarRepository.java
+++ crud-exercise/src/main/java/com/example/crudexercise/repositories/CarRepository.java
@@ -2,6 +2,8 @@ package com.example.crudexercise.repositories;
 
 import com.example.crudexercise.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car,Long> {
-}
+    }
