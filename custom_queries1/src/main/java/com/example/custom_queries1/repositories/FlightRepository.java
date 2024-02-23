1aa86369dcce960ae3b841fae43775fde262dca2
Claudio Nuccio <cnuccio00@gmail.com>
2024-02-23 14:56:53 +0100

    Fixed

--- custom_queries1/src/main/java/com/example/custom_queries1/repositories/FlightRepository.java
+++ custom_queries1/src/main/java/com/example/custom_queries1/repositories/FlightRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight,Long> {
-    @Query(value = "SELECT * FROM Flight f WHERE flight_status LIKE ONTIME",nativeQuery = true)
+    @Query(value = "SELECT * FROM Flight f WHERE flight_status LIKE 'ONTIME'",nativeQuery = true)
     List<Flight> flightStatus(FlightStatus ONTIME);
 }
