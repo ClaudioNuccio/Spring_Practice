ceeb82237a120ae1c33be759139d394b3d0b67e3
Claudio Nuccio <cnuccio00@gmail.com>
2024-03-01 22:39:40 +0100

    Added deploy_2

--- deploy_2/src/main/java/com/example/deploy_2/controllers/numberController.java
+++ deploy_2/src/main/java/com/example/deploy_2/controllers/numberController.java
@@ -1,4 +1,17 @@
 package com.example.deploy_2.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1")
 public class numberController {
+
+    @GetMapping(path = "/sum")
+    public int getRandomSum (){
+        int firstNumber = (int) (Math.random()*100);
+        int secondNUmber = (int) (Math.random()*100);
+        return firstNumber + secondNUmber;
+    }
 }
