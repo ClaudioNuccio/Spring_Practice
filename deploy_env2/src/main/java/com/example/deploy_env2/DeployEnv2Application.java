0ab13cf7dc5c7003d072ff06dc0adc7c40ce2df1
ClaudioNuccio <147241773+ClaudioNuccio@users.noreply.github.com>
2024-03-01 23:10:42 +0100

    Add files via upload

---
+++ deploy_env2/src/main/java/com/example/deploy_env2/DeployEnv2Application.java
@@ -0,0 +1,13 @@
+package com.example.deploy_env2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployEnv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployEnv2Application.class, args);
+	}
+
+}
